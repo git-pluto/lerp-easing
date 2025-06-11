52f0a76d47a208c12e70266055446110327e662f
git-pluto <alt.pluto.alt@gmail.com>
2025-06-11 18:21:07 -0300

    added the start of a "3d like 2d" that mocks up how objects further away move less and appear smaller. it should simulate looking at the horizon

--- node_2d.gd
+++ node_2d.gd
@@ -1,15 +1,23 @@
 extends Node2D
 
 @onready var icon: Sprite2D = $Icon
+var distance = 0
+var move = 0
 
 func _init() -> void:
 	Engine.time_scale = 1
-	
 
 func _process(_delta: float) -> void:
-	pass
+	distance+=move
+	icon.position.y = -200*sc.ypos(distance) # calculo + offset. esse é um tipo de funcao definitiva
+	# pra fazer um aditivo disso é so comparar o valor da posicao atual e somar/subtrair
+	icon.scale = Vector2(1,1)*sc.scale(distance)*3
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_up"):
-		var a:lerp_ease = calc.setLerp("add")
-		a.add(icon,"rotation_degrees",90,calc.tco, 0.5)
+		move=0.1
+		
+	if event.is_action_pressed("ui_down"):
+		move=-0.1
+	if event.is_action_released("ui_up") or event.is_action_released("ui_down"):
+		move=0
---
+++ space_calc.gd
@@ -0,0 +1,7 @@
+extends Node
+
+func ypos(d):
+	return (1-1.25**(-d))
+
+func scale(d):
+	return 1.1**(-d)
