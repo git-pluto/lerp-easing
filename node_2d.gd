d08c8acddf840073c211afad48825d3bbe39a246
git-pluto <alt.pluto.alt@gmail.com>
2025-06-13 00:31:24 -0300

    ok i kinda did it i just need to know what numbers i need to push to work on a normal scale... when its a normal scale its like a 1000 fov camera, everything is too "volitile". how do i tune it down?

--- node_2d.gd
+++ node_2d.gd
@@ -7,39 +7,30 @@ var move = Vector3(0,0,0)
 func _init() -> void:
 	Engine.time_scale = 1
 	await self.ready
-	sc.translate(icon,Vector3(0,0,1))
+	sc.translate(icon,Vector3(0,0,0))
 
 
 
 func _process(_delta: float) -> void:
 	pass
-	sc.translate(icon, move/100)
+	sc.translate(icon, move*5)
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_up"):
-		#var a:lerp_ease = calc.setLerp("ite") # example of simultaneous easing desync
-		#a.add(icon,"scale",0.5,calc.tco, 0.5)
-		#var b:lerp_ease = calc.setLerp("ite")
-		#b.add(icon,"scale",2,calc.co, 0.5)
-		#
-		#var c:lerp_ease = calc.setLerp("add")
-		#c.add(icon,"translation",Vector3(0,100,0),calc.hio, 1)
-		#
-		var a:lerp_ease = calc.setLerp("add")
-		a.add(icon,"translation",Vector3(0,0,-5),calc.jump, 0.5)
-		a.add(icon,"translation",Vector3(0,0, 5),calc.jump, 0.5)
-		
-		var b:lerp_ease = calc.setLerp("add")
-		b.add(icon,"translation",Vector3(200,0,0),calc.hio, 0.5)
-		b.add(icon,"translation",Vector3(-200,0,0),calc.hio, 0.5)
-		
-		
-		move.y-=1
-	if event.is_action_pressed("ui_down"):
-		move.y+=1
-	if event.is_action_pressed("ui_left"):
-		move.z-=1
-	if event.is_action_pressed("ui_right"):
-		move.z+=1
-	if event.is_action_released("ui_up") or event.is_action_released("ui_down") or event.is_action_released("ui_left") or event.is_action_released("ui_right"):
-		move = Vector3(0,0,0)
+	if event.is_action_pressed("up"):
+		move.y = -1
+	if event.is_action_pressed("down"):
+		move.y = 1
+	if event.is_action_pressed("left"):
+		move.x = -1
+	if event.is_action_pressed("right"):
+		move.x = 1
+	if event.is_action_pressed("depth in"):
+		move.z = 1
+	if event.is_action_pressed("depth out"):
+		move.z = -1
+	if not Input.is_action_pressed("left") and not Input.is_action_pressed("right"):
+		move.x = 0
+	if not Input.is_action_pressed("up") and not Input.is_action_pressed("down"):
+		move.y = 0
+	if not Input.is_action_pressed("depth in") and not Input.is_action_pressed("depth out"):
+		move.z = 0
--- space_calc.gd
+++ space_calc.gd
@@ -1,22 +1,16 @@
 extends Node
 
-func ypos(d):
-	return (1-1.1**(-d))
-
-func scale(d):
-	return 1.1**(-d)
 
 func translate(obj, move: Vector3):
-	var start: Vector3 = obj.pos
 	obj.pos+= move
 	
-	obj.position += Vector2(move.x,move.y)*2000/(((obj.pos.x**2+obj.pos.y**2+obj.pos.z**2)**0.5)+((start.x**2+start.y**2+start.z**2)**0.5))
-	#Vector2(obj.pos.x-1.1**-obj.pos.z,obj.pos.y*1.1**-obj.pos.z)-Vector2(start.x-1.1**-start.z,start.y*1.1**-start.z)
+	# how do i do this? it should be the distance from the camera, but x and y arent doing that.
 	
-	obj.scale *= Vector2(1,1)*(start.x**2+start.y**2+start.z**2)**0.5/(obj.pos.x**2+obj.pos.y**2+obj.pos.z**2)**0.5
+	obj.position = 100*Vector2(obj.pos.x,obj.pos.y)/(((obj.pos.x**2)*0.1+(obj.pos.y**2)*0.1+obj.pos.z**2)**0.5)
+
+	obj.scale = 200*Vector2(1,1)/((obj.pos.x**2+obj.pos.y**2+obj.pos.z**2)**0.5)
 	
 	# the position is still the same but the scale keeps increasing
 	#icon.position.y += pos.y*sc.ypos(distance) + pos.y*sc.ypos(distance-move) # calculo + offset. esse é um tipo de funcao definitiva
 	#icon.position.x += -(pos.x*sc.ypos(distance) + pos.x*sc.ypos(distance-move))
 	# pra fazer um aditivo disso é so comparar o valor da posicao atual e somar/subtrair
-	
